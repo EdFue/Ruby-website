7c7c85d6009d70bac6f0a4ec1de662ff53a8667e
Eduardo A. Fuentes <ed.fue.01@gmail.com>
2023-11-01 23:14:24 -0600

    Added the ISBN model validation and the model tests

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,5 @@ class Book < ApplicationRecord
   has_and_belongs_to_many :libraries
 
   validates :title, presence: true
+  validates :isbn, presence: true, format: { with: /\AISBN (\d{3}-\d{1,5}-\d{1,7}-\d{1,7}-\d{1})\z/, message: "must be a format like this: 'ISBN 978-0-306-40615-7'" }
 end
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @person = people(:one)
+    @valid_attributes = {
+      title: "A Valid Book Title",
+      isbn: "ISBN 978-0-306-40615-7",
+      person: @person
+    }
+  end
+
+  test "should be invalid without a title" do
+    book = Book.new(@valid_attributes.merge(title: ""))
+    assert_not book.valid?
+    assert_not_nil book.errors[:title]
+  end
+
+  test "should be invalid without an ISBN" do
+    book = Book.new(@valid_attributes.merge(isbn: ""))
+    assert_not book.valid?
+    assert_not_nil book.errors[:isbn]
+  end
+
+  test "should be invalid with incorrect ISBN format" do
+    book = Book.new(@valid_attributes.merge(isbn: "12345"))
+    assert_not book.valid?
+    assert_not_nil book.errors[:isbn]
+  end
 end
